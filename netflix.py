177fcc609ae232ce6c1be8e34c6cd183c0408f02
NAHHAR Mohammed <nahhar1993@gmail.com>
2024-01-05 15:07:34 +0100

    initial commit

---
+++ netflix.py
@@ -0,0 +1,10 @@
+# Import pandas ans matplotlib 
+
+import pandas as pd 
+import matplotlib.pyplot as plt 
+
+# Load file csv
+
+netflix_df= pd.read_csv(r"C:\Users\LENOVO\OneDrive\Bureau\netflix_data.csv")
+print(netflix_df)
+
