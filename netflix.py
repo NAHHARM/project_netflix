3927cb850a6c0f684089b18fc9f60b2459f9b3cb
NAHHAR Mohammed <nahhar1993@gmail.com>
2024-01-05 20:21:58 +0100

    ajout ITERATE

--- netflix.py
+++ netflix.py
@@ -1,4 +1,4 @@
-# Import pandas ans matplotlib 
+# Import pandas and matplotlib 
 
 import pandas as pd 
 import matplotlib.pyplot as plt 
@@ -10,7 +10,21 @@ netflix_df= pd.read_csv(r"C:\Users\LENOVO\OneDrive\Bureau\netflix_data.csv")
 netflix_subset= netflix_df[netflix_df["type"]=="Movie"]
 netflix_movies= netflix_subset[["title","country","genre","release_year","duration"]]
 short_movies= netflix_movies[netflix_movies["duration"]<60]
-print(short_movies[:20])
+
+# ITERATE
+colors=[]
+
+for x,y in short_movies.iterrows():
+    if y["genre"]=="Children":
+       colors.append("green")
+    elif   y["genre"]=="Documentaries":
+       colors.append("red")
+    elif y["genre"]=="Stand-Up":
+       colors.append("yellow")
+    else: 
+        colors.append("black") 
+print(colors[:20])          
+
 
 
 
