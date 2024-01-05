3c980a6d1a0d4bed8d14e7b8c85e6ebcbc010477
NAHHAR Mohammed <nahhar1993@gmail.com>
2024-01-05 15:32:17 +0100

    flitrage du data

--- netflix.py
+++ netflix.py
@@ -4,7 +4,13 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 
 # Load file csv
-
 netflix_df= pd.read_csv(r"C:\Users\LENOVO\OneDrive\Bureau\netflix_data.csv")
-print(netflix_df)
+
+# Subset data type "Movie"
+netflix_subset= netflix_df[netflix_df["type"]=="Movie"]
+netflix_movies= netflix_subset[["title","country","genre","release_year","duration"]]
+short_movies= netflix_movies[netflix_movies["duration"]<60]
+print(short_movies[:20])
+
+
 
